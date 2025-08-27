cd31bce6dd7192389126d37f526a4d655e6c5ee4
Rosa Filgueira <rosa.filgueira@ed.ac.uk>
2025-08-27 14:49:21 +0100

    Delete gitLab-impact-tracker/data_cleaning directory

--- gitLab-impact-tracker/data_cleaning/run_counting_scripts.sh
+++
@@ -1,6 +0,0 @@
-set -x
-
-python count_lines.py Persons_and_Affiliations.csv Pure_Org_Units.csv
-python count_db.py final_impact_tracker.db --org-id 1
-python3 pure_id_tools.py Persons_and_Affiliations.csv --find "Michael" "Rovatsos"
-python3 pure_id_tools.py Persons_and_Affiliations.csv --find "Rosa" "Filgueira"
